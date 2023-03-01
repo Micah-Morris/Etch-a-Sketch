361a7ac5afa4ae9a8ed3fcdb49162f645b7ed352
Micah Morris <micah.morris42.com>
2023-03-01 14:35:14 -0700

    Create function to add divs to grid

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const newBox = document.createElement('div');
-document.getElementById('container').appendChild(newBox);
+
+let gridSize = 5
+
+for (let index = 0; index < gridSize; index++) {
+    const newBox = document.createElement('div');
+    document.getElementById('container').appendChild(newBox);
+}
+
