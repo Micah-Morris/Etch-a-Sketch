38e08530a8c7783cb9bdd5a47bc49651d3875bbe
Micah Morris <micah.morris42.com>
2023-03-01 14:39:23 -0700

    Remove test divs Add 'box' class to created divs

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let gridSize = 5
 
 for (let index = 0; index < gridSize; index++) {
     const newBox = document.createElement('div');
+    newBox.classList.add('box')
     document.getElementById('container').appendChild(newBox);
 }
 
