55736e2ce911f18436a9f5a9d2d756cc30a13c3e
Micah Morris <micah.morris42.com>
2023-03-01 16:02:59 -0700

    add hover effect for one div

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 
-let gridSize = 5
+let gridSize = 16
 
 for (let index = 0; index < gridSize; index++) {
     const newBox = document.createElement('div');
     newBox.classList.add('box')
     document.getElementById('container').appendChild(newBox);
 }
+function changeColor() {
+    box.classList.add('black');
+}
 
+let box = document.querySelector('.box');
+ box.addEventListener("mouseover", function() { changeColor() });
