a9728677e191ddee347b322dc8d132c61ff8fff3
Micah Morris <micah.morris42.com>
2023-03-01 18:34:11 -0700

    add button to give size prompt make all createGrid func

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 
 let gridSize = 16
 
-
+let button = document.querySelector('.button');
+button.addEventListener('click', function() {
+   gridSize = prompt('Enter Size');
+   createGrid()
+})
+function createGrid() {
 for (let index = 0; index < gridSize; index++) {
     const newBox = document.createElement('div');
     newBox.classList.add('square')
     newBox.id = index
     document.getElementById('container').appendChild(newBox);
 }
+
+
 let box = document.querySelectorAll('.square');
 
-
-  
-  
-
- for (let index = 0; index < box.length; index++) {
+ for (let index = 0; index < gridSize; index++) {
     let i = index.toString()
     let position = document.getElementById(i);
     
@@ -24,11 +27,14 @@ let box = document.querySelectorAll('.square');
         var y = Math.floor(Math.random() * 256);
         var z = Math.floor(Math.random() * 256);
         var RGBColor = "rgb(" + x + "," + y + "," + z + ")";  
-        console.log(RGBColor);
+    
         position.style.backgroundColor = RGBColor;
         
     })
  };
+};
+createGrid();
+
  
  
 
